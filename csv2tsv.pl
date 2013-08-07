42d9f9ed766953cba8617643ddc82c99b0ed5c5a
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2013-08-08 08:36:37 +0900

    Update csv2tsv.pl

--- csv2tsv.pl
+++ csv2tsv.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 use Text::CSV_XS;
-
-my $file = "/var/log/httpd/access_log";
 my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1, sep_char=>' ' });
-open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
+my $fh = *STDIN;
 while (my $row = $csv->getline ($fh)) {
   print join("\t", @$row), "\n";
 }
